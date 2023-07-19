446a32ff1570a81f647537690a9f969d7c489d25
test <test@test.com>
2023-07-19 18:43:09 +0900

    수업

---
+++ src/main/java/com/memo/post/PostRestController.java
@@ -0,0 +1,43 @@
+package com.memo.post;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.memo.post.bo.PostBO;
+
+@RestController
+@RequestMapping("/post")
+public class PostRestController {
+	
+	@Autowired
+	private PostBO postBO;
+	
+	@PostMapping("/create")
+	public Map<String, Object> create(
+			@RequestParam("subject") String subject,
+			@RequestParam("content") String content,
+			@RequestParam(value = "file", required = false) MultipartFile file,
+			HttpSession session) {
+		
+		// 세션에서 유저 정보 받아오기
+		int userId = (int)session.getAttribute("userId");
+		
+		// DB insert
+		
+		
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 1);
+		result.put("result", "성공");
+		return result;
+	}
+
+}
